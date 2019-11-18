526eb819e0431682ed29e6e2599f70cd0304066a
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-11-18 22:18:36 +0530

    added urls to bapi

---
+++ constants/urls.py
@@ -0,0 +1,29 @@
+urls = {
+    "rootURL": "http://localhost:3200",
+    "paths": {
+        "get-user": "/get-user",
+        "add-meta-project": "/add-meta-project",
+        "add-meta-milestone": "/add-meta-milestone",
+        "add-meta-pulse": "/add-meta-pulse",
+        "add-role": "/add-role",
+        "add-locations": "/add-locations",
+        "add-project": "/add-project",
+        "add-milestone": "/add-milestone",
+        "add-pulse": "/add-pulse",
+        "get-roles": "/get-roles",
+        "get-projects": "/get-projects",
+        "get-milestones": "/get-milestones",
+        "get-pulses": "/get-pulses",
+        "get-meta-projects": "/get-meta-projects",
+        "get-meta-milestones": "/get-meta-milestones",
+        "get-meta-pulses": "/get-meta-pulses",
+        "get-my-pulses": "/get-my-pulses",
+        "update-pulse-timeline": "/update-pulse-timeline",
+        "get-locations": "/get-locations",
+        "update-base-location": "/update-base-location",
+        "add-travel": "/add-travel",
+        "add-non-availability": "/add-non-availability",
+        "get-all-milestones": "/get-all-milestones",
+        "get-user-pulses": "/get-user-pulses"
+    }
+}
