7906b55edc310a17f050f2b39adc42d1912858b8
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-11-18 22:53:53 +0530

    added add-project

---
+++ source/project/add_project.py
@@ -0,0 +1,25 @@
+import inspect
+from utils.log import logger as log
+thisFilename = __file__.split("/")[-1]
+
+from constants.urls import urls
+import requests
+
+class AddProjectResource:
+
+    def on_post(self, req, resp):
+        responseObj = {
+            "responseId": 111,
+            "message": "",
+            "data": {}
+        }
+        try:
+            url = urls["rootURL"] + urls["path"]["add-project"]
+            params = req.params
+            media = req.media
+            r = requests.post(url=url, params=params, json=media)
+            responseObj = r.json()
+        except Exception as ex:
+            log.error((thisFilename, inspect.currentframe().f_code.co_name), exc_info=True)
+            responseObj["message"] = str(ex)
+        resp.media = responseObj
