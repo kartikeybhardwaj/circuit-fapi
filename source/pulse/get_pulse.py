6a0fa91fea731ab473ca4a78ca8b99ff5e6c88c5
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-12-02 21:35:00 +0530

    added get pulse

---
+++ source/pulse/get_pulse.py
@@ -0,0 +1,24 @@
+import inspect
+from utils.log import logger as log
+thisFilename = __file__.split("/")[-1]
+
+from constants.urls import urls
+import requests
+
+class GetPulseResource:
+
+    def on_get(self, req, resp):
+        responseObj = {
+            "responseId": 111,
+            "message": "",
+            "data": {}
+        }
+        try:
+            url = urls["rootURL"] + urls["paths"]["get-pulse"]
+            params = req.params
+            r = requests.get(url=url, params=params)
+            responseObj = r.json()
+        except Exception as ex:
+            log.error((thisFilename, inspect.currentframe().f_code.co_name), exc_info=True)
+            responseObj["message"] = str(ex)
+        resp.media = responseObj
